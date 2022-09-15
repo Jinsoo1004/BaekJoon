9c4c84659e10c215d44875327986c458532b559f
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2022-09-15 11:38:30 +0900

    이게무야

---
+++ BaekJoon-10869.c
@@ -0,0 +1,11 @@
+//
+// Created by user on 2022-09-15.
+//
+#include <stdio.h>
+
+//baekjoon-10869
+int main() {
+    int a, b;
+    scanf("%d%d", &a ,&b);
+    printf("%d\n%d\n%d\n%d\n%d", a+b, a-b, a*b, a/b, a%b);
+}
---
+++ BaekJoon-10926.c
@@ -0,0 +1,12 @@
+//
+// Created by user on 2022-09-15.
+//
+#include <stdio.h>
+
+//BaekJoon-10926
+int main() {
+    char a[51];
+    scanf("%s", a);
+    printf("%s??!", a);
+    return 0;
+}
